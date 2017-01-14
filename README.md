d43c2c0ae6666ec167d1e1436c172f3d2efb600d
andre <andre@otcd.com.au>
2017-01-14 15:40:47 +1000

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+Unhide Tags on Mixcloud
+=======================
+
+Recent mixcloud.com changes have hidden the mix tags behind a rollover.
+This extension fixes this issue by exposing all of these tags.
---
+++ src/content.js
@@ -0,0 +1,4 @@
+var tag_divs = document.getElementsByClassName('dropdown anchor-right');
+for (var i = 0, l = tag_divs.length; i < l; i++) {
+  tag_divs[i].className = 'dropdown dropdown-open anchor-right';
+}
